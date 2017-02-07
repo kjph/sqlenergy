a89791b43b26f82ab3ff257b7053f27247626331
kjph <jamie.kt.phan@gmail.com>
2017-02-08 10:04:36 +1100

    bug fix for MySQL converters

--- hquery.py
+++ hquery.py
@@ -1,4 +1,25 @@
 import MySQLdb
+import datetime
+
+try:
+    import MySQLdb.converters
+except ImportError:
+    _connarg('conv')
+
+def connect_with_convert(user, passwd, host='150.229.111.139', db='ems', port=3306):
+    """
+    Specify converters for MYSQL connections
+    """
+
+    try:
+        orig_conv = MySQLdb.converters.conversions
+        conv_iter = iter(orig_conv)
+        convert = dict(zip(conv_iter, [str,] * len(orig_conv.keys())))
+        print "Connecting host=%s user=%s db=%s port=%d" % (host, user, db, port)
+        conn = MySQLdb.connect(host, user, passwd, db, port, conv=convert)
+    except MySQLdb.Error, e:
+        print "Error connecting %d: %s" % (e.args[0], e.args[1])
+    return conn
 
 def load_table_list(file):
     """
@@ -17,7 +38,7 @@ def get_query_between_dates(table_name, start_date, end_date):
     Function to generate SQL query string
     """
 
-    query = "select VALUE as KWH, FROM_UNIXTIME(TIMESTAMP/1000) AS timestamp_good FROM %s" % table_name
+    query = "select VALUE as KWH, FROM_UNIXTIME(TIMESTAMP/1000) AS timestamp_good from %s" % table_name
     query += " where ((TIMESTAMP/1000) > UNIX_TIMESTAMP('%s'))" % start_date
     query += " AND ((TIMESTAMP/1000) < UNIX_TIMESTAMP('%s'))" % end_date
 
@@ -30,16 +51,13 @@ def main():
     passwd = raw_input("Password>>\t")
 
     #Make a connection
-    db = MySQLdb.connect(host="150.229.111.139",
-                         user=user,
-                         passwd=passwd,
-                         db='ems')
+    db = connect_with_convert(user, passwd)
 
     #Get a cursor object
     cursor = db.cursor()
 
     #Specify which tables we want to run query over
-    list_of_tables = load_table_list('res/table_list.txt')
+    list_of_tables = load_table_list('res/test.txt')
 
     #Data structure to hold our results
     all_vals = []#Empty list that we will fill up
@@ -50,18 +68,15 @@ def main():
         end_date = '2016-12-16'
         query = get_query_between_dates(table, start_date, end_date)
         cursor.execute(query)
+        #print query
 
-        for value in cursor:
+        for row in cursor.fetchall():
             #Append means to ADD to a list (at the end)
-            all_vals.append((table, value))
+           print row
 
-        #Go through all values in our LIST (all_vals)
-    for result in all_vals:
-        print result
-#             table = result[0]#Extract the table name from the TUPLE
-#             value = result[1:]#Extract the value from the TUPLE
-#             string_to_write = "%s, %s\n" % (table, value)
-#             fd.write(string_to_write)
+    #     #Go through all values in our LIST (all_vals)
+    # for result in all_vals:
+    #     print result
 
 if __name__ == '__main__':
     main()
