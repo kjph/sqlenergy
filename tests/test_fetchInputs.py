4d3070a89519a201b9421e3c5df3168a09486002
Jamie Phan <jamie.kt.phan@gmail.com>
2017-02-10 00:43:29 +1100

    Restructure into Python Module

--- tests/test_fetchInputs.py
+++ tests/test_fetchInputs.py
@@ -1,11 +1,11 @@
 import unittest
-from fetchInputs import *
+from ..csiroEnergy8 import fetchInputs
 
 class TableStat(unittest.TestCase):
 
     def __init__(self, *args, **kwargs):
         super(TableStat, self).__init__(*args, **kwargs)
-        self.r = table_stat('res/test_table_list.txt')
+        self.r = fetchInputs.table_stat('./csiroEnergy8/res/test_table_list.txt')
 
     def test_output_len(self):
         self.assertTrue(len(self.r) == 4)
