2e70af7cd6126a5b3e03ad4bdfc2383a0eb0c94e
Jamie Phan <jamie.kt.phan@gmail.com>
2017-02-10 00:20:26 +1100

    fetchInputs improved testing

--- test_fetchInputs.py
+++ test_fetchInputs.py
@@ -1,15 +1,50 @@
 import unittest
 from fetchInputs import *
 
-class TypeTableMapTest(unittest.TestCase):
+class TableStat(unittest.TestCase):
 
-    def type_table_map_len(self):
-        r = load_table_list('res/test_table_list.txt')
-        self.assertTrue(len(r) == 1)
+    def __init__(self, *args, **kwargs):
+        super(TableStat, self).__init__(*args, **kwargs)
+        self.r = table_stat('res/test_table_list.txt')
 
-    def type_table_map_value(self):
-        r = load_table_list('res/test_table_list.txt')
-        self.assertTrue(r['renewable'] == ['emsj01_01_pv_pb_eastwestrealenergy'])
+    def test_output_len(self):
+        self.assertTrue(len(self.r) == 4)
+
+    def test_output_full_row(self):
+        self.assertTrue(self.r['full_row']['stype'] == 'renewable')
+        self.assertTrue(self.r['full_row']['thr_min'] == -10.0)
+        self.assertTrue(self.r['full_row']['thr_max'] == 10.0)
+        self.assertTrue(self.r['full_row']['time_format'] == '%Y-%m-%d %H:%M:%S')
+
+    def test_output_4_row(self):
+        self.assertTrue(self.r['4_row']['stype'] == 'grid')
+        self.assertTrue(self.r['4_row']['thr_min'] == 0.0)
+        self.assertTrue(self.r['4_row']['thr_max'] == 1000.0)
+        self.assertTrue(self.r['4_row']['time_format'] == '%Y-%m-%d %H:%M:%S.%f')
+
+    def test_output_3_row(self):
+        self.assertTrue(self.r['3_row']['stype'] == 'gas')
+        self.assertTrue(self.r['3_row']['thr_min'] == 0.0)
+        self.assertTrue(self.r['3_row']['thr_max'] == 100.0)
+        self.assertTrue(self.r['3_row']['time_format'] == '%Y-%m-%d %H:%M:%S.%f')
+
+    def test_output_2_row(self):
+        self.assertTrue(self.r['2_row']['stype'] == 'grid')
+        self.assertTrue(self.r['2_row']['thr_min'] == 0.0)
+        self.assertTrue(self.r['2_row']['thr_max'] == 100.0)
+        self.assertTrue(self.r['2_row']['time_format'] == '%Y-%m-%d %H:%M:%S.%f')
+
+    def test_output_bad_row(self):
+        with self.assertRaises(KeyError):
+            self.r['bad_row']['stype']
+
+    def test_types(self):
+        all_types = list(set([self.r[k]['stype'] for k in self.r]))
+        self.assertTrue(len(all_types) == 3)
+        self.assertTrue('renewable' in all_types)
+        self.assertTrue('grid' in all_types)
+        self.assertTrue('gas' in all_types)
+        self.assertTrue('solar' not in all_types)
 
 def main():
     unittest.main()
