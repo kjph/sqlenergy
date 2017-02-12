208cf8f1810e3fabcc7514f1b57dbccde1b3077b
U-NEXUS\pha087 <pha087@JHAYWARDE644-NC.nexus.csiro.au>
2017-02-13 10:43:56 +1100

    Update test scripts to reflect new module structure

--- tests/test_hquery.py
+++ tests/test_hquery.py
@@ -5,8 +5,11 @@ class GetTimeSeriesTest(unittest.TestCase):
 
     def __init__(self, *args, **kwargs):
         super(GetTimeSeriesTest, self).__init__(*args, **kwargs)
+        dbi = fetchInputs.database_inputs('./cred.json')
+        print dbi
+        tab_stat = fetchInputs.table_stat('./tests/res/test_table_list.txt')
         self.TestSeries = hquery.get_time_series('2016-01-01', '2016-01-02', 15,
-                                                 './sqlenergy/res/test_table_list.txt')
+                                                 tab_stat, **dbi)
 
     def test_return_class_stat(self):
         self.assertTrue(len(self.TestSeries.all_types) == 1)
@@ -17,29 +20,29 @@ class GetTimeSeriesTest(unittest.TestCase):
         self.TestSeries.fill_time_series()
 
         #Test minimum time
-        time_min_exp = hquery.get_datetime_from_str('2016-01-01 00:15:00')
+        time_min_exp = fetchInputs.get_datetime_from_str('2016-01-01 00:15:00')
         self.assertTrue(self.TestSeries.time_min == time_min_exp)
 
         #Test maximum time
-        time_max_exp = hquery.get_datetime_from_str('2016-01-01 23:45:00')
+        time_max_exp = fetchInputs.get_datetime_from_str('2016-01-01 23:45:00')
         self.assertTrue(self.TestSeries.time_max == time_max_exp)
 
     def test_negative_recovery(self):
 
         #Negative value
-        key = hquery.get_datetime_from_str('2016-01-01 10:45:00')
+        key = fetchInputs.get_datetime_from_str('2016-01-01 10:45:00')
         self.assertTrue(self.TestSeries.time_value_map['renewable'][key] == 0)
 
         #Post negative value
-        key = hquery.get_datetime_from_str('2016-01-01 11:00:00')
+        key = fetchInputs.get_datetime_from_str('2016-01-01 11:00:00')
         self.assertTrue(self.TestSeries.time_value_map['renewable'][key] == 8.25634765625)
 
         #Negative value
-        key = hquery.get_datetime_from_str('2016-01-01 12:30:00')
+        key = fetchInputs.get_datetime_from_str('2016-01-01 12:30:00')
         self.assertTrue(self.TestSeries.time_value_map['renewable'][key] == 0)
 
         #Post negative value
-        key = hquery.get_datetime_from_str('2016-01-01 12:45:00')
+        key = fetchInputs.get_datetime_from_str('2016-01-01 12:45:00')
         self.assertTrue(self.TestSeries.time_value_map['renewable'][key] == 9.85595703125)
 
 def main():
