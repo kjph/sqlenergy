3696e2ea4db6e5f0046e0700ad0aca2d1e51b355
Jamie Phan <jamie.kt.phan@gmail.com>
2017-02-13 14:23:48 +1100

    Setuptools config

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
-from setuptools import setup
+from distutils.core import setup
 
 setup(name='sqlenergy',
+      packages=['sqlenergy'],
       version='0.1',
       description='SQL wrapper for Energy Consumption Analytics',
       url='https://github.com/kjph/sqlenergy',
       author='Jamie Phan',
       author_email='jamie.kt.phan@gmail.com',
-      license='mit',
-      packages=['sqlenergy'],
-      keywords=['energy'])
+      license='mit')
